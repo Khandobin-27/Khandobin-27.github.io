bb4cfc557a0cd5fecddf1a1882c8cba6280cc9a9
Khandobin-27 <62336858+Khandobin-27@users.noreply.github.com>
2020-03-27 00:22:01 +0200

    Uploading Website

---
+++ script.js
@@ -0,0 +1,66 @@
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
+
+
+// var card = document.getElementById("card");
+
+// card.addEventListener("click", function() {
+//     card.classList.add("back-side");
+//     var header = document.createElement("h2");
+//     header.append(document.createTextNode("Email: vladislav-depo2@yandex.ru"));
+//     card.append(header);
+// })
+
+
+
+
+
+
+
+
+// <!-- Navbar-->
+// <nav class="navbar navbar-light light-blue lighten-4">
+
+//     <!-- Navbar brand -->
+//     <a class="navbar-brand" href="#">Navigation</a>
+
+//     <!-- Collapse button -->
+//     <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+//     aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
+//     class="fas fa-bars fa-1x"></i></span></button>
+
+//     <!-- Collapsible content -->
+//     <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+
+//     <!-- Links -->
+//     <ul class="navbar-nav mr-auto">
+//         <li class="nav-item active">
+//             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link" href="#">About</a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link" href="#">Education</a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link" href="#">Experience</a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link" href="#">Contacts</a>
+//         </li>
+//     </ul>
+//     <!-- Links -->
+
+//     </div>
+// <!-- Collapsible content -->
+
+// </nav>
+// <!--/.Navbar
